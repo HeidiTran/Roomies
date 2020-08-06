fc304d4db4b598983789757c5ba97a2b4fffa3cd
Heidi Tran <HeidiTran1410@gmail.com>
2020-08-06 11:14:41 -0400

    Move database connection to local

--- roomies-server/account.js
+++ roomies-server/account.js
@@ -1,9 +1,27 @@
+const pg = require("pg");
+const env = require("./env.json");
+const Pool = pg.Pool;
+const pool = new Pool(env);
+
+/**
+ * Connect to roomies database
+ */
+pool.connect().then(function () {
+  console.log(`Connected to database ${env.database}`);
+});
+
 module.exports = createNewUserAccount = (req, res) => {
   // TODO: Validation: check body has username, password, etc
   // TODO: Check if username already exists
   // TODO: Insert into DB (status 500 if fail to insert)
   // TODO: Send empty response with status 200 if success
   console.log("createNewUserAccount function runs");
+
+  // Just an example. You can safely delete this
+  pool.query("SELECT * from users").then(function (response) {
+    console.log(response);
+  });
+
   return res.status(200).send();
 };
 
