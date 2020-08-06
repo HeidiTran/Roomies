3348009da3ab2d4a4d41cfba2e4fa3b460ab779f
Eileen Moroz <egm46@cs.drexel.edu>
2020-08-06 13:08:45 -0400

    Adding Sign In Component Form

--- roomies-client/src/app/sign-in/sign-in.component.ts
+++ roomies-client/src/app/sign-in/sign-in.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-sign-in',
@@ -6,10 +7,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sign-in.component.css']
 })
 export class SignInComponent implements OnInit {
-
-  constructor() { }
+  signInForm: FormGroup;
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit() {
+    this.signInForm = this.formBuilder.group({
+      username: ['', Validators.required],
+      password: ['', [Validators.required, Validators.minLength(8)]]
+    });
   }
 
+  get form() { return this.signInForm.controls; }
+
+  onSubmit() {
+    // TODO: call the service to send the form to backend
+    console.log(this.signInForm.value);
+
+    // TODO: if success: redirect to home dashboard
+    // TODO: if fail: alert the user and reset form
+  }
 }
+
+
