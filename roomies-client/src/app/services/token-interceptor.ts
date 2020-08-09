565e16f0b5a0fe319b913073e3d130e50ee1d317
Heidi Tran <HeidiTran1410@gmail.com>
2020-08-09 13:05:48 -0400

    Add angular services for authenticate user

---
+++ roomies-client/src/app/services/token-interceptor.ts
@@ -0,0 +1,58 @@
+import { Injectable } from "@angular/core";
+import { Observable, throwError } from "rxjs";
+import { map, catchError } from "rxjs/operators";
+import {
+  HttpInterceptor,
+  HttpRequest,
+  HttpHandler,
+  HttpEvent,
+  HttpResponse,
+} from "@angular/common/http";
+
+@Injectable()
+export class TokenInterceptor implements HttpInterceptor {
+  intercept(
+    request: HttpRequest<any>,
+    next: HttpHandler
+  ): Observable<HttpEvent<any>> {
+    const token = localStorage.getItem("userAccessToken");
+    if (token) {
+      request = request.clone({
+        setHeaders: {
+          Authorization: "Bearer " + token,
+        },
+      });
+    }
+
+    request = request.clone({
+      headers: request.headers.set("Accept", "application/json"),
+    });
+
+    return next.handle(request).pipe(
+      map((event: HttpEvent<any>) => {
+        if (event instanceof HttpResponse) {
+          console.log("event--->>>", event);
+        }
+        return event;
+      }),
+      catchError(err => {
+        if (err.status === 401) {
+          console.log(
+            "Unauthorized! Please re-enter your email and password to login!"
+          );
+          return;
+        } else if (err.status === 400) {
+          console.log("Please check the form for any input errors!");
+          return;
+        } else if (err.status === 403) {
+            console.log("Forbiden!");
+            return;
+        }
+
+        const error = err.error.message || err.statusText;
+        console.log(error);
+        return throwError(error);
+      })
+    );
+  }
+}
