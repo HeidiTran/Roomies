e580964b3b97f04b50165a7512341261f30dcdeb
Eileen Moroz <egm46@cs.drexel.edu>
2020-08-11 11:07:17 -0400

    Created add grocery item form

--- roomies-client/src/app/add-grocery-item/add-grocery-item.component.ts
+++ roomies-client/src/app/add-grocery-item/add-grocery-item.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-add-grocery-item',
@@ -6,10 +7,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-grocery-item.component.css']
 })
 export class AddGroceryItemComponent implements OnInit {
-
-  constructor() { }
+  addGroceryItemForm: FormGroup;
+  
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit() {
+    this.addGroceryItemForm = this.formBuilder.group({
+      itemName: ['', Validators.required],
+      quantity: [1,Validators.min(1)],
+      price: [0, Validators.required, Validators.min(.01), Validators.pattern('^[0-9]*\.[0-9]{2}$ or ^[0-9]*\.[0-9][0-9]$')]
+    });
   }
 
+  get from(){ return this.addGroceryItemForm.controls; }
+
+  onSubmit(){
+    // TODO: call the service to send the form item to backend
+    console.log(this.addGroceryItemForm.value);
+
+    // TODO: if success: redirect to grocery list
+    // TODO: if fail: alert the user and reset form
+  }
 }
--- roomies-client/src/app/app-routing.module.ts
+++ roomies-client/src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { CreateAccountComponent } from './create-account/create-account.component';
 import { SignInComponent } from './sign-in/sign-in.component';
+import { AddGroceryItemComponent } from './add-grocery-item/add-grocery-item.component';
 
 
 const routes: Routes = [
   { path: 'newAccount', component: CreateAccountComponent },
   { path: 'signIn', component: SignInComponent },
+  { path: 'addGroceryItem', component: AddGroceryItemComponent},
 ];
 
 @NgModule({
