12d5682a2ece1b9f0f3f24145f4274db1d9ce118
Eileen Moroz <egm46@cs.drexel.edu>
2020-08-14 18:19:19 -0400

    Added Grocery List Table to Dashboard Component

--- roomies-client/src/app/dashboard/dashboard.component.ts
+++ roomies-client/src/app/dashboard/dashboard.component.ts
@@ -5,9 +5,47 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css']
 })
-export class DashboardComponent implements OnInit {
 
-  constructor() { }
+export class DashboardComponent implements OnInit {
+  groceries: Object[];
+
+  constructor() {
+    this.groceries = [
+      {
+        itemname: "Bread",
+        price: "5.00",
+        quantity: 4
+      },
+      {
+        itemname: "Eggs",
+        price: "2.25",
+        quantity: 4
+      },
+      {
+        itemname: "Avocados",
+        price: "3.99",
+        quantity: 4
+      },
+    ];
+   }
+   private addGroceryItem = false;
+   loadMyChildComponent(){
+      this.addGroceryItem = true;
+   }
+
+   removeItem(grocery){
+     console.log(this);
+     let deleteItem = confirm('Are you sure you want to delete '+grocery.itemname+'?');
+      
+     if (deleteItem){
+       console.log("deleting"+ grocery);
+       //TODO: remove grocery item from DB
+       
+     }
+     else{
+       console.log("Not deleting" + grocery);
+     }
+   }
 
   ngOnInit() {
   }
