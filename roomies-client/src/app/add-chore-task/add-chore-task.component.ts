f41d288e2862b216d79accad1d492b04356e9376
Eileen Moroz <egm46@cs.drexel.edu>
2020-08-14 21:42:40 -0400

    Created add chore task component form

--- roomies-client/src/app/add-chore-task/add-chore-task.component.ts
+++ roomies-client/src/app/add-chore-task/add-chore-task.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-add-chore-task',
@@ -6,10 +7,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-chore-task.component.css']
 })
 export class AddChoreTaskComponent implements OnInit {
+  addChoreTaskForm: FormGroup;
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit() {
+    this.addChoreTaskForm = this.formBuilder.group({
+      taskName: ['', Validators.required],
+    });
   }
 
+  get form(){ return this.addChoreTaskForm.controls; }
+
+  onSubmit(){
+    // TODO: call the service to send the form task item to backend
+    console.log(this.addChoreTaskForm.value);
+
+    // TODO: if success: redirect to task list
+    // TODO: if fail: alert the user and reset form
+  }
 }
