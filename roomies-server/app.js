9a96828999b298f4384efe2934d17bfc2d8cb1d3
Heidi Tran <HeidiTran1410@gmail.com>
2020-08-20 16:11:31 -0400

    Add allow Authorization header to fix CORS

--- roomies-server/app.js
+++ roomies-server/app.js
@@ -12,7 +12,7 @@ app.use(express.json());
 // Config CORS
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
   next();
 });
 
