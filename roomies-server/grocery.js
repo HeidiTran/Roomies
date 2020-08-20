3c0fb2ac52c009c178ba2ebfa7ad3cf4b8bdd933
Neel <neel.jagad1@gmail.com>
2020-08-20 19:26:52 -0400

    Retested functions and implemented returns to spec

--- roomies-server/grocery.js
+++ roomies-server/grocery.js
@@ -16,7 +16,6 @@ function parseIntValidation(object) {
 	if(object){
 		intObj = parseInt(object)
 		strObj = intObj.toString()
-		console.log(intObj)
 		if (strObj.length === object.length){
 			return intObj;
 		}
@@ -25,7 +24,6 @@ function parseIntValidation(object) {
 		}
 	}
 	else{
-		console.log("no")
 		return false
 	}
   } catch (error) {
@@ -37,46 +35,42 @@ module.exports = getAllItems = async (req, res) => {
   // TODO: check if houseId is in the request query
 	
 	let houseId = parseIntValidation(req.query.houseId);
-	console.log(houseId);
 	if (houseId){
 		try {
-			console.log("here?");
 			const { rows } = await pool.query(
 			  "SELECT * FROM Items where house_id = $1",
 			  [houseId]
 			);
 
 			if (rows.length > 0){
-				return res.status(200).json({"Items":rows})
+				return res.status(200).json(rows)
 			}
 			else{
-				return res.status(500).send({});
+				return res.status(500).send();
 			}
 	  } catch (error) {
-		  console.log(error)
-		  return res.status(500).send({});
+		  return res.status(500).send();
 		}
 		
 	}
 	else{
-		return res.status(500).send({});
+		return res.status(500).send();
 	}
 	
 };
 
 module.exports = getItem = async (req, res) => {
-	console.log(req);
 	if (
 		!req.body.hasOwnProperty("itemId") ||
 		!req.body.hasOwnProperty("houseId")
 	  ) {
-		return res.status(500).send({});
+		return res.status(500).send();
 	  }
 	if (
 		typeof req.body.itemId != "number" ||
 		typeof req.body.houseId != "number"
 		) {
-		return res.status(500).send({});
+		return res.status(500).send();
 	  }
 	  
 	try {
@@ -86,19 +80,17 @@ module.exports = getItem = async (req, res) => {
 		);
 
 		if (rows.length > 0){
-			return res.status(200).json({"Items":rows})
+			return res.status(200).json(rows[0])
 		}
 		else{
-			return res.status(500).send({});
+			return res.status(500).send();
 		}
   } catch (error) {
-	  console.log(error);
-	  return res.status(500).send({});
+	  return res.status(500).send();
 	  }
 };
 
 module.exports = addItem = async (req, res) => {
-	console.log("hellu");
 	if (
 		!req.body.hasOwnProperty("houseId") ||
 		!req.body.hasOwnProperty("name") ||
@@ -115,17 +107,16 @@ module.exports = addItem = async (req, res) => {
 		typeof req.body.price != "number" ||
 		typeof req.body.bought != "boolean"
 		) {
-		return res.status(500).send({});
+		return res.status(500).send();
 	  }
 	try {
 		await pool.query(
 		  "INSERT INTO Items (name, house_id, quantity, price, bought) VALUES ($1, $2, $3, $4, $5)",
 		  [req.body.name, req.body.houseId, req.body.quantity, req.body.price,req.body.bought ]
 		);
-		return res.status(201).send({});
+		return res.status(201).send();
 	  } catch (error) {
-		  console.log(error);
-		  return res.status(500).send({});
+		  return res.status(500).send();
 	  }
 		
 };
@@ -140,18 +131,18 @@ module.exports = editItem = async (req, res) => {
 		!req.body.hasOwnProperty("houseId") ||
 		!req.body.hasOwnProperty("itemId")
 	  ) {
-		return res.status(500).send({});
+		return res.status(500).send();
 	  }
 	if (
 		typeof req.body.name != "string" ||
 		typeof req.body.quantity != "number" ||
 		typeof req.body.price != "number" ||
 		typeof req.body.bought != "boolean" ||
-		typeof req.body.boughtOn != "object" ||
+		typeof req.body.boughtOn != "string" ||
 		typeof req.body.houseId != "number" ||
 		typeof req.body.itemId != "number"
 		) {
-		return res.status(500).send({});
+		return res.status(500).send();
 	  }
 	try {
     await pool.query(
@@ -160,14 +151,13 @@ module.exports = editItem = async (req, res) => {
 			quantity = $2,\
 			price = $3,\
 			bought = $4,\
-			bought_on = $5,\
+			bought_on = $5\
 		WHERE item_id = $6 and house_id = $7;",
       [req.body.name, req.body.quantity, req.body.price, req.body.bought, req.body.boughtOn, req.body.itemId, req.body.houseId]
     );
-    return res.status(200).send({});
+    return res.status(200).send();
   } catch (error) {
-	  console.log(error);
-	  return res.status(500).send({});
+	  return res.status(500).send();
   }
 };
 
@@ -177,22 +167,21 @@ module.exports = deleteItem = async (req, res) => {
 			!req.body.hasOwnProperty("itemId") ||
 			!req.body.hasOwnProperty("houseId")
 		  ) {
-			return res.status(500).send({});
+			return res.status(500).send();
 		  }
 		if (
 			typeof req.body.itemId != "number" ||
 			typeof req.body.houseId != "number"
 			) {
-			return res.status(500).send({});
+			return res.status(500).send();
 		  }
 			await pool.query(
 			   "DELETE FROM Items\
 				WHERE item_id = $1 and house_id = $2;",
 			  [req.body.itemId, req.body.houseId]
 			);
-		return res.status(200).send({});
+		return res.status(200).send();
   } catch (error) {
-	  console.log(error);
-	  return res.status(500).send({});
+	  return res.status(500).send();
   }
 };
