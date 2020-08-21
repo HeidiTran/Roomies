185a5ad1d195ac421eb94e23546143d4fbe582b5
Heidi Tran <HeidiTran1410@gmail.com>
2020-08-21 10:19:27 -0400

    Update return JSON for getAllItems

--- roomies-server/grocery.js
+++ roomies-server/grocery.js
@@ -11,21 +11,19 @@ pool.connect();
 function parseIntValidation(object) {
   try {
     let intObj;
-	let strObj;
-	
-	if(object){
-		intObj = parseInt(object)
-		strObj = intObj.toString()
-		if (strObj.length === object.length){
-			return intObj;
-		}
-		else{
-			return false
-		}
-	}
-	else{
-		return false
-	}
+    let strObj;
+
+    if (object) {
+      intObj = parseInt(object);
+      strObj = intObj.toString();
+      if (strObj.length === object.length) {
+        return intObj;
+      } else {
+        return false;
+      }
+    } else {
+      return false;
+    }
   } catch (error) {
     throw Error(error);
   }
@@ -33,155 +31,176 @@ function parseIntValidation(object) {
 
 module.exports = getAllItems = async (req, res) => {
   // TODO: check if houseId is in the request query
-	
-	let houseId = parseIntValidation(req.query.houseId);
-	if (houseId){
-		try {
-			const { rows } = await pool.query(
-			  "SELECT * FROM Items where house_id = $1",
-			  [houseId]
-			);
 
-			if (rows.length > 0){
-				return res.status(200).json(rows)
-			}
-			else{
-				return res.status(500).send();
-			}
-	  } catch (error) {
-		  return res.status(500).send();
-		}
-		
-	}
-	else{
-		return res.status(500).send();
-	}
-	
+  let houseId = parseIntValidation(req.query.houseId);
+  if (houseId) {
+    try {
+      const {
+        rows,
+      } = await pool.query("SELECT * FROM Items where house_id = $1", [
+        houseId,
+      ]);
+
+      if (rows.length > 0) {
+        return res.status(200).json(
+          rows.map((elem) => {
+            return {
+              itemId: elem.item_id,
+              name: elem.name,
+              quantity: elem.quantity,
+              price: elem.price,
+              bought: elem.bought,
+            };
+          })
+        );
+      } else {
+        return res.status(200).json([]);
+      }
+    } catch (error) {
+      return res.status(500).send();
+    }
+  } else {
+    return res.status(500).send("Error while getting houseId!");
+  }
 };
 
 module.exports = getItem = async (req, res) => {
-	if (
-		!req.body.hasOwnProperty("itemId") ||
-		!req.body.hasOwnProperty("houseId")
-	  ) {
-		return res.status(500).send();
-	  }
-	if (
-		typeof req.body.itemId != "number" ||
-		typeof req.body.houseId != "number"
-		) {
-		return res.status(500).send();
-	  }
-	  
-	try {
-		const { rows } = await pool.query(
-		  "SELECT * FROM Items where item_id = $1 and house_id = $2",
-		  [req.body.itemId, req.body.houseId]
-		);
+  if (
+    !req.body.hasOwnProperty("itemId") ||
+    !req.body.hasOwnProperty("houseId")
+  ) {
+    return res.status(500).send();
+  }
+  if (
+    typeof req.body.itemId != "number" ||
+    typeof req.body.houseId != "number"
+  ) {
+    return res.status(500).send();
+  }
 
-		if (rows.length > 0){
-			return res.status(200).json(rows[0])
-		}
-		else{
-			return res.status(500).send();
-		}
+  try {
+    const {
+      rows,
+    } = await pool.query(
+      "SELECT * FROM Items where item_id = $1 and house_id = $2",
+      [req.body.itemId, req.body.houseId]
+    );
+
+    if (rows.length > 0) {
+      return res.status(200).json(rows[0]);
+    } else {
+      return res.status(500).send();
+    }
   } catch (error) {
-	  return res.status(500).send();
-	  }
+    return res.status(500).send();
+  }
 };
 
 module.exports = addItem = async (req, res) => {
-	if (
-		!req.body.hasOwnProperty("houseId") ||
-		!req.body.hasOwnProperty("name") ||
-		!req.body.hasOwnProperty("quantity") ||
-		!req.body.hasOwnProperty("price") ||
-		!req.body.hasOwnProperty("bought")
-	  ) {
-		return res.status(500).send({});
-	  }
-	if (
-		typeof req.body.houseId != "number" ||
-		typeof req.body.name != "string" ||
-		typeof req.body.quantity != "number" ||
-		typeof req.body.price != "number" ||
-		typeof req.body.bought != "boolean"
-		) {
-		return res.status(500).send();
-	  }
-	try {
-		await pool.query(
-		  "INSERT INTO Items (name, house_id, quantity, price, bought) VALUES ($1, $2, $3, $4, $5)",
-		  [req.body.name, req.body.houseId, req.body.quantity, req.body.price,req.body.bought ]
-		);
-		return res.status(201).send();
-	  } catch (error) {
-		  return res.status(500).send();
-	  }
-		
+  if (
+    !req.body.hasOwnProperty("houseId") ||
+    !req.body.hasOwnProperty("name") ||
+    !req.body.hasOwnProperty("quantity") ||
+    !req.body.hasOwnProperty("price") ||
+    !req.body.hasOwnProperty("bought")
+  ) {
+    return res.status(500).send({});
+  }
+  if (
+    typeof req.body.houseId != "number" ||
+    typeof req.body.name != "string" ||
+    typeof req.body.quantity != "number" ||
+    typeof req.body.price != "number" ||
+    typeof req.body.bought != "boolean"
+  ) {
+    return res.status(500).send();
+  }
+  try {
+    await pool.query(
+      "INSERT INTO Items (name, house_id, quantity, price, bought) VALUES ($1, $2, $3, $4, $5)",
+      [
+        req.body.name,
+        req.body.houseId,
+        req.body.quantity,
+        req.body.price,
+        req.body.bought,
+      ]
+    );
+    return res.status(201).send();
+  } catch (error) {
+    return res.status(500).send();
+  }
 };
 
 module.exports = editItem = async (req, res) => {
-	if (
-		!req.body.hasOwnProperty("name") ||
-		!req.body.hasOwnProperty("quantity") ||
-		!req.body.hasOwnProperty("price") ||
-		!req.body.hasOwnProperty("bought") ||
-		!req.body.hasOwnProperty("boughtOn") ||
-		!req.body.hasOwnProperty("houseId") ||
-		!req.body.hasOwnProperty("itemId")
-	  ) {
-		return res.status(500).send();
-	  }
-	if (
-		typeof req.body.name != "string" ||
-		typeof req.body.quantity != "number" ||
-		typeof req.body.price != "number" ||
-		typeof req.body.bought != "boolean" ||
-		typeof req.body.boughtOn != "string" ||
-		typeof req.body.houseId != "number" ||
-		typeof req.body.itemId != "number"
-		) {
-		return res.status(500).send();
-	  }
-	try {
+  if (
+    !req.body.hasOwnProperty("name") ||
+    !req.body.hasOwnProperty("quantity") ||
+    !req.body.hasOwnProperty("price") ||
+    !req.body.hasOwnProperty("bought") ||
+    !req.body.hasOwnProperty("boughtOn") ||
+    !req.body.hasOwnProperty("houseId") ||
+    !req.body.hasOwnProperty("itemId")
+  ) {
+    return res.status(500).send();
+  }
+  if (
+    typeof req.body.name != "string" ||
+    typeof req.body.quantity != "number" ||
+    typeof req.body.price != "number" ||
+    typeof req.body.bought != "boolean" ||
+    typeof req.body.boughtOn != "string" ||
+    typeof req.body.houseId != "number" ||
+    typeof req.body.itemId != "number"
+  ) {
+    return res.status(500).send();
+  }
+  try {
     await pool.query(
-       "UPDATE Items\
+      "UPDATE Items\
 		SET name = $1,\
 			quantity = $2,\
 			price = $3,\
 			bought = $4,\
 			bought_on = $5\
 		WHERE item_id = $6 and house_id = $7;",
-      [req.body.name, req.body.quantity, req.body.price, req.body.bought, req.body.boughtOn, req.body.itemId, req.body.houseId]
+      [
+        req.body.name,
+        req.body.quantity,
+        req.body.price,
+        req.body.bought,
+        req.body.boughtOn,
+        req.body.itemId,
+        req.body.houseId,
+      ]
     );
     return res.status(200).send();
   } catch (error) {
-	  return res.status(500).send();
+    return res.status(500).send();
   }
 };
 
 module.exports = deleteItem = async (req, res) => {
-	try {
-		if (
-			!req.body.hasOwnProperty("itemId") ||
-			!req.body.hasOwnProperty("houseId")
-		  ) {
-			return res.status(500).send();
-		  }
-		if (
-			typeof req.body.itemId != "number" ||
-			typeof req.body.houseId != "number"
-			) {
-			return res.status(500).send();
-		  }
-			await pool.query(
-			   "DELETE FROM Items\
+  try {
+    if (
+      !req.body.hasOwnProperty("itemId") ||
+      !req.body.hasOwnProperty("houseId")
+    ) {
+      return res.status(500).send();
+    }
+    if (
+      typeof req.body.itemId != "number" ||
+      typeof req.body.houseId != "number"
+    ) {
+      return res.status(500).send();
+    }
+    await pool.query(
+      "DELETE FROM Items\
 				WHERE item_id = $1 and house_id = $2;",
-			  [req.body.itemId, req.body.houseId]
-			);
-		return res.status(200).send();
+      [req.body.itemId, req.body.houseId]
+    );
+    return res.status(200).send();
   } catch (error) {
-	  return res.status(500).send();
+    return res.status(500).send();
   }
 };
